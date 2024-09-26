8150055114478de8d3083302fce74b9a26d6fbe0
SimoLomba53 <simolomba253@gmail.com>
2024-09-26 13:06:10 +0200

    JAVASCRIPT MENU

--- script.js
+++ script.js
@@ -15,7 +15,53 @@ document.addEventListener("DOMContentLoaded", () => {
     let isAnimating = false;
 
     
-   
+   const splitTextIntoSpans = (selector) => {
+    let elements = document.querySelectorAll(selector);
+    elements.forEach((element) => {
+       let text = element.innerText;
+       let splitText = text
+       .split("")
+       .map(function (char){
+          return `<span>${char === "" ? "&nbsp;&nbsp;" : char}</span>`;
+       })
+    .join("");
+    element.innerHTML = splitText;
+
+    });
+   };
+
+   splitTextIntoSpans(".header h1")
 
 
-})
+   .menuToggle.addEventListener("click", () => {
+        
+      if (isAnimating) return;
+
+      if (menuToggle.classList.contains("closed")){
+        menuToggle.classList.remove("closed");
+        menuToggle.classList.add("opened");
+
+        isAnimating = true;
+
+        gsap.to(menu, {
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            ease : "hop",
+            duration: 1.5,
+            onStart: () => {
+                menu.computedStyleMap.pointerEvents = "all";
+            },
+            onComplete: () => {
+                isAnimating = false;
+            }
+        })
+
+      }else{
+        menuToggle.classList.remove("opened");
+        menuToggle.classList.add("closed");
+
+        isAnimating= true;
+      }
+
+
+   });
+});
